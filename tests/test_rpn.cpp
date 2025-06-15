8a534fad7ff25518f42d8deba37feef989709c65
Grzegorz Ożański <greggor@gmail.com>
2025-06-15 14:16:59 +0200

    Get rid of '<empty namespace>' Test Explorer message

--- tests/test_rpn.cpp
+++ tests/test_rpn.cpp
@@ -16,19 +16,19 @@ bool run_test(const std::string& input, const std::string& output) {
     return (output.find("Error") != std::string::npos ? "" : rpn::prompt) + output + "\n" == (err.str().empty() ? out.str() : err.str());
 }
 
-TEST_CASE("Arithmetic.Simple") {
+TEST_CASE("Tests.Simple.SimpleArithmetic") {
     REQUIRE(run_test("3 4 +", "Result: 7"));
     REQUIRE(run_test("10 2 /", "Result: 5"));
     REQUIRE(run_test("5 3 -", "Result: 2"));
     REQUIRE(run_test("6 7 *", "Result: 42"));
 }
 
-TEST_CASE("Expressions.Complex") {
+TEST_CASE("Tests.Simple.ComplexExpressions") {
     REQUIRE(run_test("2 3 + 4 *", "Result: 20"));
     REQUIRE(run_test("5 1 2 + 4 * + 3 -", "Result: 14"));
 }
 
-TEST_CASE("Errors.Errors") {
+TEST_CASE("Tests.Simple.Errors") {
     REQUIRE(run_test("", "Error: No input provided"));
     REQUIRE(run_test("3 +", "Error: Too few operands"));
     REQUIRE(run_test("3 2 1 +", "Error: Invalid expression"));
@@ -36,7 +36,7 @@ TEST_CASE("Errors.Errors") {
     REQUIRE(run_test("xyz", "Error: Invalid token"));
 }
 
-TEST_CASE("Constants.Constants") {
+TEST_CASE("Tests.Simple.Constants") {
     REQUIRE(run_test("pi", "Result: 3.14159"));
     REQUIRE(run_test("-pi", "Result: -3.14159"));
     REQUIRE(run_test("e", "Result: 2.71828"));
